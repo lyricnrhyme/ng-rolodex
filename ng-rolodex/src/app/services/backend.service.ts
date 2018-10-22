a67785cced9ddf0681949466e851508e2bc5f665
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro-2.local>
2018-10-21 23:49:01 -1000

    Got Search Bar Working, added Card, Add Form, and Edit Form components

---
+++ ng-rolodex/src/app/services/backend.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class BackendService {
+    testNames: object[] = [{name:'Jyama'}, {name:'Sarah'}, {name:'Jamie'}, {name:'Wymin'}, {name:'Nick'}, {name:'Doug'}, {name:'Abby'}, {name:'Carl'}, {name:'Jason'}, {name:'Romeo'}, {name:'Jeff'}, {name:'May'}, {name:'Harsh'}, {name:'Chaz'}, {name:'Raymond'}, {name:'Baseem'}, {name:'Ed'}, {name: 'Kenny'}, {name: 'Jesus'}, {name: 'Bronson'}, {name: 'George'}, {name: 'McKarl'}, {name: 'Isaiah'}];
+
+    constructor(private http: HttpClient) {
+
+    }
+
+    // getCharacter(id: number) {
+    //     const url = this.baseUrl + 'people/' + id;
+    //     return this.http.get(url).toPromise();
+    // }
+
+    // addCharacter(character) {
+    //     this.characters.push(character);
+    // }
+
+}
