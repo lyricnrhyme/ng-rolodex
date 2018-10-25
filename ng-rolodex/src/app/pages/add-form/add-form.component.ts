c60a8c046b5ae22c67242180b8bab222af63abd9
Jamie Yamashita <jamieyamashita@Jamies-MBP-2.home>
2018-10-24 18:23:33 -1000

    Added in Create New Contact function, edited addForm

--- ng-rolodex/src/app/pages/add-form/add-form.component.ts
+++ ng-rolodex/src/app/pages/add-form/add-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BackendService } from '../../services/backend.service';
 
 @Component({
   selector: 'app-add-form',
@@ -6,10 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-form.component.scss']
 })
 export class AddFormComponent implements OnInit {
+  testNames: object[]
 
-  constructor() { }
+  formData: {
+    name: string,
+    address: string,
+    mobile: string,
+    work: string,
+    home: string,
+    email: string,
+    twiter: string,
+    instagram: string,
+    github: string
+  } = {
+    name: '',
+    address: '',
+    mobile: '',
+    work: '',
+    home: '',
+    email: '',
+    twiter: '',
+    instagram: '',
+    github: ''
+  }
+
+  constructor(private backend: BackendService) { }
+
+  addContact() {
+    this.testNames.push({
+      name: this.formData.name,
+      address: this.formData.address,
+      mobile: this.formData.mobile,
+      work: this.formData.work,
+      home: this.formData.home,
+      email: this.formData.email,
+      twiter: this.formData.twiter,
+      instagram: this.formData.instagram,
+      github: this.formData.github
+    })
+    console.log('testing', this.testNames);
+  }
 
   ngOnInit() {
+    this.testNames = this.backend.testNames
   }
 
 }
